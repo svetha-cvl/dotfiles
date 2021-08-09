ac632bc013aa0c4276001823d5c8c6124862a39e
Svetha CVL <svetha.cvl@gmail.com>
2021-08-09 21:31:58 +0530

    Add a few convenience functions and autoload them in ZSH config (#4)

--- install.sh
+++ install.sh
@@ -12,11 +12,12 @@ if ! test -f git/.gitconfig.local.symlink; then
     sed -e "s/AUTHOR/$git_author/g" -e "s/EMAIL/$git_email/g" git/.gitconfig.symlink > git/.gitconfig.local.symlink
 fi
 
+sed -e "s=DOTFILES_ROOT=${DOTFILES_ROOT}=g" zsh/.zshrc.symlink > zsh/.zshrc.local.symlink
+
 ln $DOTFILES_ROOT/git/.gitconfig.local.symlink ~/.gitconfig
 
 ln $DOTFILES_ROOT/psql/.psqlrc.symlink ~/.psqlrc
 
 ln $DOTFILES_ROOT/vim/.vimrc.symlink ~/.vimrc
 
-
-ln $DOTFILES_ROOT/zsh/.zshrc.symlink ~/.zshrc
+ln $DOTFILES_ROOT/zsh/.zshrc.local.symlink ~/.zshrc
