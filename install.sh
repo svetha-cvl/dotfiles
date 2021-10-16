9f339be73024778c913a0f553101ab37da429425
Svetha <svetha.cvl@gmail.com>
2021-10-16 21:14:20 +0530

    Shellcheck suggestions for installation script Double quote literals to handle spaces

--- install.sh
+++ install.sh
@@ -15,16 +15,16 @@ sed -e "s=DOTFILES_ROOT=${DOTFILES_ROOT}=g" zsh/.zshrc.symlink > zsh/.zshrc.loca
 
 # git autocompletion
 # Sourced from https://github.com/git/git/blob/v2.33.0/contrib/completion/git-completion.zsh. Credit to authors.
-curl https://raw.githubusercontent.com/git/git/v2.33.0/contrib/completion/git-completion.bash -o $DOTFILES_ROOT/zsh/_git-completion-bash
-curl https://raw.githubusercontent.com/git/git/v2.33.0/contrib/completion/git-completion.zsh -o $DOTFILES_ROOT/zsh/_git-completion-zsh
+curl https://raw.githubusercontent.com/git/git/v2.33.0/contrib/completion/git-completion.bash -o "$DOTFILES_ROOT/zsh/_git-completion-bash"
+curl https://raw.githubusercontent.com/git/git/v2.33.0/contrib/completion/git-completion.zsh -o "$DOTFILES_ROOT/zsh/_git-completion-zsh"
 
-ln $DOTFILES_ROOT/git/.gitconfig.local.symlink ~/.gitconfig
+ln "$DOTFILES_ROOT/git/.gitconfig.local.symlink" ~/.gitconfig
 
-ln $DOTFILES_ROOT/psql/.psqlrc.symlink ~/.psqlrc
+ln "$DOTFILES_ROOT/psql/.psqlrc.symlink" ~/.psqlrc
 
-ln $DOTFILES_ROOT/vim/.vimrc.symlink ~/.vimrc
-ln $DOTFILES_ROOT/vim/coc-settings.json ~/.config/nvim/coc-settings.json
-ln $DOTFILES_ROOT/vim/init.vim ~/.config/nvim/init.vim
+ln "$DOTFILES_ROOT/vim/.vimrc.symlink" ~/.vimrc
+ln "$DOTFILES_ROOT/vim/coc-settings.json" ~/.config/nvim/coc-settings.json
+ln "$DOTFILES_ROOT/vim/init.vim" ~/.config/nvim/init.vim
 
-ln $DOTFILES_ROOT/zsh/.p10k.zsh ~/.p10k.zsh
-ln $DOTFILES_ROOT/zsh/.zshrc.local.symlink ~/.zshrc
+ln "$DOTFILES_ROOT/zsh/.p10k.zsh" ~/.p10k.zsh
+ln "$DOTFILES_ROOT/zsh/.zshrc.local.symlink" ~/.zshrc
