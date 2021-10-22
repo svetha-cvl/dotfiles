eb01375a735eb3ec2cf18470b2c2a85d0408ad80
Svetha <svetha.cvl@gmail.com>
2021-10-22 10:50:22 +0530

    Add zsh-autosuggestions plugin - Install plugin automatically if not present - Plugin source: https://github.com/zsh-users/zsh-autosuggestions

--- install.sh
+++ install.sh
@@ -20,8 +20,13 @@ if [ ! -f "$DOTFILES_ROOT/zsh/_git-completion-bash" ]; then
 fi
 
 # https://github.com/zsh-users/zsh-syntax-highlighting
-if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+fi
+
+# https://github.com/zsh-users/zsh-autosuggestions
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 fi
 
 ln "$DOTFILES_ROOT/git/.gitconfig.local.symlink" ~/.gitconfig
